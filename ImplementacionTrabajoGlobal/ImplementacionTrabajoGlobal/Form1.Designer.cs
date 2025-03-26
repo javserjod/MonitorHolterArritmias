ed240175c585e4c1fc1a5b01a0b788ca937dd7c7
Javier Serrano Jodral <151475665+javserjod@users.noreply.github.com>
2025-03-26 01:28:20 +0100

    Add files via upload

---
+++ ImplementacionTrabajoGlobal/ImplementacionTrabajoGlobal/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ImplementacionTrabajoGlobal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(28, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Comenzar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(495, 384);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(582, 45);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.InterlacedColor = System.Drawing.Color.White;
+            chartArea1.AxisX.LabelStyle.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.LightCoral;
+            chartArea1.AxisX.MajorGrid.LineWidth = 3;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisX.MinorGrid.Enabled = true;
+            chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.LightCoral;
+            chartArea1.AxisX.Title = "Tiempo (cs)";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.LightCoral;
+            chartArea1.AxisY.MajorGrid.LineWidth = 3;
+            chartArea1.AxisY.MajorTickMark.Enabled = false;
+            chartArea1.AxisY.MinorGrid.Enabled = true;
+            chartArea1.AxisY.MinorGrid.LineColor = System.Drawing.Color.LightCoral;
+            chartArea1.AxisY.Title = "Voltaje ECG (µV)";
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            chartArea1.BackColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Black;
+            series1.Name = "Canal ECG";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1065, 315);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "200 Hz";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "500 Hz";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(23, 68);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "1000 Hz";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(181, 336);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Frecuencia de Muestreo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Location = new System.Drawing.Point(351, 336);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(117, 100);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Umbral Onda R";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(25, 22);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(59, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.Text = "700 µV";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Checked = true;
+            this.radioButton5.Location = new System.Drawing.Point(25, 45);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(65, 17);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "1200 µV";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(25, 68);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(65, 17);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.Text = "1500 µV";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(525, 349);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Pulsaciones por minuto en reposo:";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(28, 396);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 33);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Guardar ECG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1089, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ImplementacionTrabajoGlobal/ImplementacionTrabajoGlobal/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+
+// Implementación de Monitor Holter con actuación ante arritmias
+// Realizado por Javier Serrano Jodral
+
+// El programa detecta si las pulsaciones por minuto del usuario, recibidas mediante Bitalino, están en el rango normal, 
+// y en caso contraro, indica si se trata de Bradicardia o Taquicardia, ambas consideradas tipos de Arritmia.
+
+
+namespace ImplementacionTrabajoGlobal
+{
+    public partial class Form1 : Form
+    {
+        Bitalino dev;
+        Bitalino.Frame[] frames;    // Vector de datos recibidos del Bitalino
+        public int umbralOndaR;     // Valor umbral en µV para detectar picos ondas R y poder medir intervalo RR = Ritmo cardíaco
+        List<int> cumpleUmbral = new List<int>();    // Lista para detección de picos, o sea, onda R -> Valores 1 y 0
+        int banderaContador = 0;      // Bandera para obviar en los cálculos las primeras muestras (hasta la primera onda R), ya que no son precisas
+        int contador = 0;             // Conteo de muestras entre onda R y la siguiente onda R
+        List<int> bufferBpm = new List<int>();    // Buffer con todos los tiempos de cada intervalo RR
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            dev = new Bitalino("COM6");   // Inicializar variable dispositivo y asignar puerto Bluetooth
+            int nDatos = 2;      // Cantidad de datos que se reciben cada vez que se lee el dispositivo
+                                  // Tras varias pruebas, recibir 1 dato con una de las frecuencias ofrecidas es suficiente y va muy fluido
+            frames = new Bitalino.Frame[nDatos];
+            for (int i=0; i< frames.Length; i++)
+            {
+                frames[i] = new Bitalino.Frame();
+            }
+
+            textBox1.Text = "MODO DE USO: " +
+                "Seleccione una frecuencia de muestreo, que representa la cantidad de veces por segundo que se lee el sensor. " +
+                "Indique el umbral para distinguir los picos de las ondas R y poder calcular intervalo RR. " +
+                "Haga click en Comenzar. Tenga en cuenta que los cambios en los botones solo afectarán a la siguiente ejecución.";
+
+            // Código para poder hacer zoom en ECG
+            ChartArea CA = chart1.ChartAreas[0];
+            CA.AxisX.ScaleView.Zoomable = true;
+            CA.CursorX.AutoScroll = true;
+            CA.CursorX.IsUserSelectionEnabled = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)    // Botón Comenzar/Finalizar
+        {
+            if (timer1.Enabled == false)   // Está apagado y se ha hecho click en Comenzar
+            {
+                button1.Text = "Finalizar";
+                textBox1.Text = "Mediciones en curso. Recuerde que puede hacer zoom en el ECG obtenido arrastrando y soltando el ratón. " +
+                    "Haga click en Finalizar para concluir el ECG.";
+
+                // Asignar duración del timer en milisegundos, a partir de botón marcado -> Frecuencia = 1000/timer.Interval   (Hz)
+                if (radioButton1.Checked)
+                {
+                    timer1.Interval = 5;            // 200 Hz (por defecto)
+                }
+                else if (radioButton2.Checked)
+                {
+                    timer1.Interval = 2;            // 500 Hz 
+                }
+                else if (radioButton3.Checked)
+                {
+                    timer1.Interval = 1;            // 1000 Hz 
+                }
+
+                // Asignar valor umbral, en µV
+                if (radioButton4.Checked)   
+                {
+                    umbralOndaR = 700;              // (por defecto)
+                }
+                else if (radioButton5.Checked)
+                {
+                    umbralOndaR = 1200;            
+                }
+                else if (radioButton6.Checked)   
+                {
+                    umbralOndaR = 2000;              
+                }
+
+                // Línea que representa al umbral, para mejor comprensión visual
+                chart1.ChartAreas["ChartArea1"].AxisY.StripLines.Clear();
+                StripLine stripline = new StripLine();
+                stripline.Interval = 0;
+                stripline.IntervalOffset = umbralOndaR;
+                stripline.StripWidth = 2;
+                stripline.BackColor = Color.Blue;
+                chart1.ChartAreas["ChartArea1"].AxisY.StripLines.Add(stripline);
+
+
+                textBox1.Text = "Mediciones en curso. Haga click en Finalizar para concluir el ECG. " +
+                    "Frecuencia de muestreo actual: " + 1000/timer1.Interval + " Hz. Umbral para detección de picos: "+umbralOndaR+ " µV.";
+
+                chart1.Series[0].Points.Clear();   // Reseteo del chart
+
+                dev.start(100, new int[] { 0 });   // Iniciar dispositivo. Frecuencia de lectura de sensores, Vector con entradas a leer
+                timer1.Enabled = true;             // Habilitar timer una vez iniado el dispositivo
+
+                button2.Visible = false;           // Ocultar botón de descarga del ECG
+            }
+            else    // timer1.Enabled == true  ---> Está midiendo y se ha clicado en Finalizar
+            {
+                button1.Text = "Comenzar";
+                textBox1.Text = "Fin de las mediciones. Puede descargar una captura de la curva clicando en el botón de la izquierda Guardar ECG. " +
+                    "Haga click en Comenzar si desea iniciar un nuevo ECG.";
+
+                dev.stop();                        // Desconectar dispositivo
+                timer1.Enabled = false;            // Apagar timer
+
+                cumpleUmbral.Clear();    // Vaciar lista de detección de picos
+                banderaContador = 0;     // Poner a 0 bandera
+                contador = 0;            // Reiniciar contador
+                bufferBpm.Clear();       // Vaciar buffer de latidos por minuto
+
+                button2.Visible = true;    // Mostrar botón de descarga del ECG
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Se lee del dispositivo cada vez que concluye un intervalo del timer
+            dev.read(frames);
+
+            //Console.WriteLine(frames[0].analog[0]);
+
+            int valorNuevo;    // Variable para almacenar datos recopilados y ajustarlos
+
+
+            for (int i=0; i<frames.Length; i++)
+            {
+                valorNuevo = (frames[i].analog[0] - 500) * 10;     // Ajustar datos para que se centren en 0, y voltaje en microvoltios (µV)
+
+                cumpleUmbral.Add(valorNuevo>=umbralOndaR?1:0);         // Añadir 1 o 0, si el valor actual supera o no el umbral
+
+                if (cumpleUmbral.Count >= 2)       // Si ya se han evaluado dos valores (el primero y el actual)...
+                {
+                    if ((cumpleUmbral[cumpleUmbral.Count - 2] == 0) & (cumpleUmbral[cumpleUmbral.Count - 1] == 1))    // Si anterior fue  0 y actual 1 ("subida escalón")  _-
+                    {                
+                        if (banderaContador == 1)   // Si ya vale 1, se ha cumplido un ciclo desde la onda R anterior
+                        {
+                            // Console.WriteLine((100 * 60) / contador);    // Mostrar conversión de muestras contadas a latidos por minuto
+                            bufferBpm.Add((100*60)/contador);      // Añadir al buffer latidos por minuto, tras conversión
+                            if (bufferBpm[bufferBpm.Count - 1] < 60)         // Bradicardia detectada
+                            {
+                                label1.Text = "Pulsaciones por minuto en reposo: " + bufferBpm[bufferBpm.Count - 1] + " < 60 -> Bradicardia";
+                                label1.BackColor = Color.IndianRed;
+                            }
+                            else if (bufferBpm[bufferBpm.Count - 1] > 100)   // Taquicardia detectada
+                            {
+                                if (bufferBpm[bufferBpm.Count - 1] <= 200)   // Valores demasiado elevados: ERROR en medición
+                                {
+                                    label1.Text = "Pulsaciones por minuto en reposo: " + bufferBpm[bufferBpm.Count - 1] + " > 100 -> Taquicardia";
+                                    label1.BackColor = Color.IndianRed;
+                                }
+                                else
+                                {
+                                    label1.Text = "Pulsaciones por minuto en reposo: ? -> Error en la medición";
+                                    label1.BackColor = Color.IndianRed;
+                                }
+                            }     
+                            else    // Latidos dentro del rango sano
+                            {
+                                label1.Text = "Pulsaciones por minuto en reposo: " + bufferBpm[bufferBpm.Count - 1] + " -> Latido normal";
+                                label1.BackColor = Color.Lime;
+                            }
+
+                            //bufferBpm.Average();          // Calcular la media de pulsaciones por minuto hasta el momento
+                            contador = 0;          // Reiniciar contador para usarlo en el próximo ciclo cardíaco
+                        }
+                        banderaContador = 1;       // Poner bandera a 1 en el primer escalón (forma de descartar los primeros valores, que presentan mucho ruido). Luego siempre vale 1
+                    }
+                    if (banderaContador == 1) 
+                    { 
+                        contador++;  // Aumenta nDatos veces cada vez que se consulta el sensor. Se para al hallar otro escalón.
+                    }
+                }
+
+                chart1.Series[0].Points.Add(valorNuevo);     // Añadir valores a la gráfica, convertidos a microvoltios (µV)
+            
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)   // Botón de guardar imagen ECG
+        {
+            DateTime time = DateTime.Now;
+
+            string nombreImg = String.Format("ECG_{0}{1}{2}_{3}{4}{5}", 
+                time.Day, time.Month, time.Year, time.Second, time.Minute, time.Hour);    // Formato nombre archivo guardado
+
+            string carpeta = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Environment.CurrentDirectory))));   // Directorio
+
+            string pathImage = carpeta +  "\\" + nombreImg + ".png";
+
+            chart1.SaveImage(pathImage, ChartImageFormat.Png);
+
+            MessageBox.Show("Imagen guardada: " + nombreImg + ".png");    // Mostrar mensaje de guardado con éxito
+        }
+    }
+}
